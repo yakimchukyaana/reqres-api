f1648c15c7b0bc42482d500509b16d7e10fa4069
Яна Якимчук <jerrrytyson@Anas-MacBook-Air.local>
2023-11-24 17:28:16 +0800

    use api config

--- src/test/java/in/reqres/tests/TestBase.java
+++ src/test/java/in/reqres/tests/TestBase.java
@@ -1,14 +1,16 @@
 package in.reqres.tests;
 
+import in.reqres.config.ApiConfig;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+    static ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class);
+
     @BeforeAll
-    static void beforeAll(){
-        RestAssured.baseURI = "https://reqres.in";
-        RestAssured.basePath = "/api";
+    public static void beforeAll(){
+        RestAssured.baseURI = apiConfig.BaseUri();
+        RestAssured.basePath = apiConfig.BasePath();
     }
-
-
 }
